8c1c68ff65008d57bbfab3d4dae3ba62ea67d343
Lehan Gajanayake <gajanayakelehan@gmail.com>
2020-05-06 22:48:53 +0530

    / func

--- Restapi/Main/service/user.go
+++ Restapi/Main/service/user.go
@@ -1,2 +1,16 @@
 package service
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+//StartServer starts the rest api
+func StartServer(addr string, port int)error{
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello world")
+	})
+	return nil 
+}
