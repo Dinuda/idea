9216bfc9897c402461f0aba0f0a32baa81abec41
Lehan Gajanayake <gajanayakelehan@gmail.com>
2020-05-07 11:06:20 +0530

    remove cognito

--- Restapi/Main/repository/user.go
+++ Restapi/Main/repository/user.go
@@ -6,14 +6,14 @@ import(
 
 	//Driver to intrigate with MySql
 	_ "github.com/go-sql-driver/mysql"
-	cognito "github.com/aws/aws-sdk-go/service/cognitoidentity"
+	//cognito "github.com/aws/aws-sdk-go/service/cognitoidentity"
 
 	"../models"
 )
 //DB DB connection
 var DB *sql.DB
 
-func cognito(tokens)
+//func cognito(tokens)
 
 //Connect is used to connect to the db
 func Connect(path string)(*sql.DB, error){
