0af3d9ee2416b2228000c10267da7c0a22250e19
Lehan Gajanayake <gajanayakelehan@gmail.com>
2020-05-21 12:50:43 +0530

    simple

--- backend/Main/models/models.go
+++ backend/Main/models/models.go
@@ -2,46 +2,46 @@ package models
 
 //User new User
 type User struct {
-	Username    string
-	Password    string
-	Name        string
-	PhoneNo     int
-	Address     string
-	Email       string
-	Description string
-	Type        string
-	Investor    Investor
-	Student     Student
+	Username    string   `json:"username,omitempty"`
+	Password    string   `json:"password,omitempty"`
+	Name        string   `json:"name,omitempty"`
+	PhoneNo     int      `json:"phone_no,omitempty"`
+	Address     string   `json:"address,omitempty"`
+	Email       string   `json:"email,omitempty"`
+	Description string   `json:"description,omitempty"`
+	Type        string   `json:"type,omitempty"`
+	Investor    Investor `json:"investor,omitempty"`
+	Student     Student  `json:"student,omitempty"`
 }
 
 //Student  new Student
 type Student struct {
-	ID         int
-	UserID     int
-	TeamID     int
-	Profession int
-	CV         string
-	TeamRole   string
+	ID         int    `json:"id,omitempty"`
+	UserID     int    `json:"user_id,omitempty"`
+	TeamID     int    `json:"team_id,omitempty"`
+	Profession int    `json:"profession,omitempty"`
+	CV         string `json:"cv,omitempty"`
+	TeamRole   string `json:"team_role,omitempty"`
 }
 
 //Investor new investor
 type Investor struct {
-	ID       int
-	UserID   int
-	Linkedin string
-	Company  string
-	Projects []int
+	ID       int    `json:"id,omitempty"`
+	UserID   int    `json:"user_id,omitempty"`
+	Linkedin string `json:"linkedin,omitempty"`
+	Company  string `json:"company,omitempty"`
+	Projects []int  `json:"projects,omitempty"`
 }
 
 //Project new host of project
 type Project struct {
-	ID          int
-	Title       string
-	Description string
-	CreatedDate string
-	ClosedDate  string
-	Category    int //Agriculture, IT,
-	Host      int
+	ID          int    `json:"id,omitempty"`
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+	CreatedDate string `json:"created_date,omitempty"`
+	ClosedDate  string `json:"closed_date,omitempty"`
+	Category    int    `json:"category,omitempty"` //Agriculture, IT,
+	Host        int    `json:"host,omitempty"`
 }
 
 //StudentTeam is used to make a team by students
@@ -60,14 +60,14 @@ type InvestorTeam struct {
 
 //Profession to get the professions
 type Profession struct {
-	ID   int
-	Name string
+	ID   int    `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
 }
 
 //ProjectCategory catagories of the project
 type ProjectCategory struct {
-	ID   int
-	Name string
+	ID   int    `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
 }
 
 // 	Date  int
